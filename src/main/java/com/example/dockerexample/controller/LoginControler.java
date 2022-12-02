09e4ebe183fa29d19e97599b9feb621e1045b8ca
LoanNguyenThi5 <Loan.NguyenThi5@nashtechglobal.com>
2022-12-02 16:58:17 +0700

    test

--- src/main/java/com/example/dockerexample/controller/LoginControler.java
+++ src/main/java/com/example/dockerexample/controller/LoginControler.java
@@ -19,6 +19,7 @@ import com.example.dockerexample.entity.UserAdmin;
 @RestController
 public class LoginControler {
 
+    // test
     @Autowired
     private AuthenticationManager authenticationManager;
 
